1d11a332a01ae5b1052d3801fb1b18732c5d6f07
HowardKIM2 <mihaelkel@naver.com>
2018-04-07 01:55:02 -0700

    cp

--- 18th/5.c
+++ 18th/5.c
@@ -8,17 +8,15 @@ int main(int argc, char **argv){
 	char buf[1024];
 	int i;
 	if(argc != 3){
-		printf("인자 3개 입력!!\n");
+		printf("use : %s <origin> <copied>\n",argv[0]);
 		exit(-1);
 	}
-	for(i = 0; i < argc; i++)
-		printf("인자 : %s\n",argv[i]);
 	
 	fdin = open(argv[1],O_RDONLY);
 	fdout = open(argv[2],O_WRONLY | O_CREAT | O_TRUNC | O_EXCL, 0644);
 
 	while((nread = read(fdin,buf,1024)) > 0){
-		printf("Success : %ld\n",nread);
+		printf("Success : %ld byte copied %s -> %s\n",nread,argv[1],argv[2]);
 		if(write(fdout, buf ,nread) < nread){
 			printf("ERROR : \"%s\" already exists\n",argv[2]);
 			close(fdin);
