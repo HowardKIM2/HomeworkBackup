64760e90ed277df815f455aee6ebec5e492517b3
HowardKIM2 <mihaelkel@naver.com>
2018-04-16 21:57:19 -0700

    fifo

--- 20th/5.c
+++ 20th/5.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
-
+#include <string.h>
+void create_file(void);
 int main(void){
 	int fd, ret;
 	char buf[1024];
 	fd = open("myfifo",O_RDWR);
-	fcntl(0, F_SETFL, O_NONBLOCK);
-	fcntl(fd, F_SETFL, O_NONBLOCK);
+//	fcntl(0, F_SETFL, O_NONBLOCK);
+//	fcntl(fd, F_SETFL, O_NONBLOCK);
 
 	int i = 0;
 	for(i = 0;;i++){
@@ -21,10 +22,18 @@ int main(void){
 			buf[ret - 1] = 0;
 			printf("Pipe Input : [%s]\n",buf);
 		}
-		if(0 == i % 500000)
-			printf("non-blocking!\n");
+		if(!strcmp(buf,"c type.c"))
+			create_file();
 	}
 	close(fd);
 
 	return 0;
+}
+void create_file(void){
+	int fd;
+	if((fd = open("type.c",O_CREAT|O_TRUNC,0644)) > 0)
+		printf("type.c created!\n");
+	else
+		printf("create error\n");
+	close(fd);
 }
